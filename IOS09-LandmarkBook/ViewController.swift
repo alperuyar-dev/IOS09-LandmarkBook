ac3f929b7d9287aef2f7664d8c8194b726f07615
Alper Uyar <alperuyar.dev@gmail.com>
2022-09-25 13:09:45 +0300

    Initial Commit

---
+++ IOS09-LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS09-LandmarkBook
+//
+//  Created by Alper Uyar on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
